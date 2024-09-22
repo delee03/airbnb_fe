2738934dae83bd11d4c0789f4e1f8e623318f79d
Toan Phan <toanphangl1997@gmail.com>
2024-09-22 14:44:18 +0700

    updateSignIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import SignInSignUp from "./pages/SignInSignUp";
+
+import UserInformation from "./pages/UserInformation/UserInformation";
+
 function App() {
-    return (
-        <div className="text-red-500 text-center text-2xl py-10">
-            Đây là Teamplate Boilerplate react version 18
-        </div>
-    );
+  return <SignInSignUp />;
 }
 
 export default App;
---
+++ src/service/utils/utils.js
@@ -0,0 +1,9 @@
+// export function setLocalStorage(key, value) {
+//   const data = typeof value === "string" ? value : JSON.stringify(value);
+//   localStorage.setItem(key, data);
+// }
+
+// export function getLocalStorage(key) {
+//   const dataLocal = localStorage.getItem(key);
+//   return dataLocal ? JSON.parse(dataLocal) : null;
+// }
