416e1256f7c3fb4c53391493148c6e410b845c03
Pham Phat <phamphat2003vn@gmail.com>
2024-09-28 11:05:27 +0700

    fix update user // debug with formik

--- src/service/nguoiDung.service.js
+++ src/service/nguoiDung.service.js
@@ -1,20 +1,21 @@
 import { http } from "./config";
 
 export const nguoiDungService = {
-  getListUser: async () => {
-    return await http.get("/users");
-  },
-  // NV2 : Tạo hàm xử lí gọi API xóa người dùng
-  // DELETE
-  deleteUser: (id) => {
-    return http.delete(`/users?id=${id}`);
-  },
-  // POST
-  createUser: (data) => {
-    return http.post("/users", data);
-  },
-  // PUT
-  updateUser: async (id, data) => {
-    return await http.put(`/users/${id}`, data);
-  },
+    getListUser: async () => {
+        return await http.get("/users");
+    },
+    // NV2 : Tạo hàm xử lí gọi API xóa người dùng
+    // DELETE
+    deleteUser: (id) => {
+        return http.delete(`/users?id=${id}`);
+    },
+    // POST
+    createUser: (data) => {
+        return http.post("/users", data);
+    },
+    // PUT
+    updateUser: async (id, data) => {
+        console.log("Update User Params:", id, data); // Check params before sending request
+        return await http.put(`/users/${id}`, data);
+    },
 };
